077e25654375345eb1441e9448bdcea089f1ea06
ARSHIOSDK <arshiosdk@gmail.com>
2024-03-22 10:23:02 +0100

    Added express and created routes POST and Get

--- index.js
+++ index.js
@@ -1,6 +1,34 @@
 var http = require('http');
+var express = require('express');
 
-var app = http.createServer((req, res) => {
-        res.end("Hello World");
+var app = express();
+
+app.use((req,res,next) => {
+    console.log("Inside the first function")
+    next();
+})
+
+app.get("/user", (req, res, next) => {
+    let user = {
+        name : 'Arshdeep',
+        age  : '27',
+        gender: 'male',
+    };
+    res.status(200).json({
+        msg: 1,
+        user: user
+    });
 });
-app.listen(4000);
+
+app.post("/user", (req,res, next) => {
+    res.status(200).json({
+        msg: 1,
+        data: 'User has been inserted successfully'
+    });
+});
+
+
+
+app.listen(4001);
+
+
