65f73c9dbac5337719a7e56f64606adca85ea71f
ARSHIOSDK <arshiosdk@gmail.com>
2024-03-24 12:52:01 +0100

    Create Update and Delete and finding single user using phone no

--- app/controllers/User.js
+++ app/controllers/User.js
@@ -19,6 +19,63 @@ module.exports.getAllUsers = (req, res, next) => {
     });
 }
 
+module.exports.findUser = async (req, res, next) => {
+    try {
+        // Assuming UserModel has a method named findOne to find a user by phone number
+        const user = await UserModel.findOne({ phone: req.params.phone });
+        if (!user) {
+            // If no user found, return a 404 error response
+            FUNCTION.error({
+                status: 404,
+                msg: 'User not found.',
+            }, res);
+            return;
+        }
+        // If user found, return a success response with the user data
+        FUNCTION.success({
+            status: 200,
+            data: user
+        }, res);
+    } catch (err) {
+        // If an error occurs, return a 422 error response with the error details
+        FUNCTION.error({
+            status: 422,
+            error: err.errors,
+        }, res);
+    }
+};
+
+
+
+module.exports.updateUser = async (req, res, next) => {
+    try {
+        // Assuming UserModel has a method named findOneAndUpdate to find and update a user
+        const updatedUser = await UserModel.findOneAndUpdate(
+            { phone: req.params.phone }, // Find user by phone number
+            { $set: req.body }, // Update user with data from request body
+            { new: true } // Return the updated user
+        );
+        if (!updatedUser) {
+            // If no user found, return an error response
+            FUNCTION.error({
+                status: 404,
+                msg: 'User not found.',
+            }, res);
+            return;
+        }
+        FUNCTION.success({
+            status: 200,
+            msg: 'User has been updated successfully.',
+            data: updatedUser
+        }, res);
+    } catch (err) {
+        FUNCTION.error({
+            status: 422,
+            error: err.errors,
+        }, res);
+    }
+};
+
 module.exports.save = async (req, res, next) => {
     let user = new UserModel ({
         phone: req.body.phone,
@@ -40,4 +97,29 @@ module.exports.save = async (req, res, next) => {
             error: err.errors,
         }, res);
     });
+};
+
+module.exports.deleteUser = async (req, res, next) => {
+    try {
+        // Assuming UserModel has a method named findOneAndDelete to find and delete a user
+        const deletedUser = await UserModel.findOneAndDelete({ phone: req.body.phone });
+        if (!deletedUser) {
+            // If no user found, return an error response
+            FUNCTION.error({
+                status: 404,
+                msg: 'User not found.',
+            }, res);
+            return;
+        }
+        FUNCTION.success({
+            status: 200,
+            msg: 'User has been deleted successfully.',
+            data: deletedUser
+        }, res);
+    } catch (err) {
+        FUNCTION.error({
+            status: 422,
+            error: err.errors,
+        }, res);
+    }
 };
--- app/routers/UserRoutes.js
+++ app/routers/UserRoutes.js
@@ -3,8 +3,11 @@ const UserController = require('../controllers/User');
 
 const router = express.Router();
 
-router.get("/users",UserController.getAllUsers);
 router.post("/user", UserController.save);
+router.get("/users",UserController.getAllUsers);
+router.get('/users2/:phone', UserController.findUser);
+router.put("/updateUser/:phone",UserController.updateUser);
+router.delete("/delete",UserController.deleteUser);router.post("/user", UserController.save);
 
 
 module.exports = router;
